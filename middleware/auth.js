931d6afea9b091f3f3fe9f969c70c63134133603
rubiyadav <rubiflyeis@gmail.com>
2023-05-22 18:36:46 +0530

    update

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,26 +1,23 @@
 const jwt = require("jsonwebtoken");
-const User = require('../models/user_model');
-const catchAsyncError = require('./AsyncError')
+const User = require("../models/user_model");
+const catchAsyncError = require("./AsyncError");
 const ErrorHander = require("../utils/errorhander");
 exports.isAuthenticatedUser = catchAsyncError(async (req, res, next) => {
-    try{
-    const token = req.get("Authorization")?.split("Bearer ")[1];
-    //console.log(token)
-  if (!token) {
-    return next(new ErrorHander("Please Login to access this resource", 401));
-  }
-
-  const decodedData = jwt.verify(token, process.env.SECRET);
-  console.log(decodedData)
-
-  req.user = await User.findById(decodedData._id);
-
-  next();
-}catch(error){
-    return res.status(400).json({
-        errorName: error.name,
-        errorMessage: error.message
-
-    })
-}
-})
+    try {
+        const token = req.get("Authorization")?.split("Bearer ")[1];
+        //console.log(token)
+        if (!token) {
+            return next(
+                new ErrorHander("Please Login to access this resource", 401)
+            );
+        }
+        const decodedData = jwt.verify(token, process.env.SECRET);
+        req.user = await User.findById(decodedData.phone);
+        next();
+    } catch (error) {
+        return res.status(400).json({
+            errorName: error.name,
+            errorMessage: error.message,
+        });
+    }
+});
